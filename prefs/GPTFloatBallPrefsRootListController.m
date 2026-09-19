180efee66b7f98ff2c0bac10b1d475fa75d92ddb
lingyangxue <lingyangxue@gmail.com>
2026-09-19 22:44:18 +0800

    Update GPTFloatBallPrefsRootListController.m

--- prefs/GPTFloatBallPrefsRootListController.m
+++ prefs/GPTFloatBallPrefsRootListController.m
@@ -1,11 +1,10 @@
 #import <Preferences/PSListController.h>
 #import <spawn.h>
-#import <stdlib.h>
 
-// ==== 原来在 .h 里的 interface，现在内联到这里 ====
+// ==== 原来在 .h 里的 interface，内联到这里 ====
 @interface GPTFloatBallPrefsRootListController : PSListController
 @end
-// ==================================================
+// ==============================================
 
 #define kPrefsPlist @"/var/mobile/Library/Preferences/com.yourname.gptfloatball.plist"
 
@@ -53,7 +52,6 @@
             return;
         }
     }
-    system("killall -9 SpringBoard");
 }
 
 @end
