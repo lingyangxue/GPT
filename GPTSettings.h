642112880f665698cc14a19999d144a61202ba16
lingyangxue <lingyangxue@gmail.com>
2026-09-20 18:19:39 +0800

    Add new properties to GPTSettings interface

--- GPTSettings.h
+++ GPTSettings.h
@@ -1,21 +1,23 @@
 #import <Foundation/Foundation.h>
-
-#define kPrefsDomain  @"com.yourname.gptfloatball"
-
+#import <UIKit/UIKit.h>
+#define kPrefsDomain @"com.yourname.gptfloatball"
 static inline NSString *prefsPath(void) {
     return @"/var/mobile/Library/Preferences/com.yourname.gptfloatball.plist";
 }
-
 @interface GPTSettings : NSObject
 + (BOOL)isEnabled;
 + (NSString *)apiKey;
 + (NSString *)apiBaseURL;
 + (NSString *)modelName;
 + (NSString *)iconPath;
-+ (NSString *)systemPrompt;       // 新增
-+ (double)temperature;            // 新增
-+ (NSInteger)maxTokens;           // 新增
++ (NSString *)systemPrompt;
++ (double)temperature;
++ (NSInteger)maxTokens;
++ (CGFloat)ballSize;
++ (CGFloat)ballOpacity;
++ (CGFloat)windowScale;
 + (NSArray *)chatHistory;
 + (void)saveChatHistory:(NSArray *)history;
 + (void)clearHistory;
++ (void)setValue:(id)value forKey:(NSString *)key;
 @end
