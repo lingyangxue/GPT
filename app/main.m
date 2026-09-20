6993f70ca6ae7e85b4bf3fae3549d3cf88f61af0
lingyangxue <lingyangxue@gmail.com>
2026-09-20 19:57:40 +0800

    Create main.m

---
+++ app/main.m
@@ -0,0 +1,23 @@
+#import <UIKit/UIKit.h>
+#import "ChatViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (nonatomic, strong) UIWindow *window;
+@end
+
+@implementation AppDelegate
+- (BOOL)application:(UIApplication *)app didFinishLaunchingWithOptions:(NSDictionary *)opts {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    ChatViewController *vc = [ChatViewController new];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
+    self.window.rootViewController = nav;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+@end
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
